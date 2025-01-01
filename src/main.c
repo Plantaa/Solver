d3e6568da719a8122611e8a877c5fac3c8084db4
Victor <vcabredo@gmail.com>
2025-01-01 14:59:39 -0300

    MODIFY: Test using table

--- src/main.c
+++ src/main.c
@@ -3,43 +3,30 @@
 #include <time.h>
 
 #include "deck.h"
+#include "player.h"
+#include "table.h"
 
 int main()
 {
     srand((unsigned int)time(0));
 
-    Deck originalDeck, shuffledDeck;
+    Deck originalDeck;
     deckInit(&originalDeck);
 
-    deckInit(&shuffledDeck);
-    fisherYatesShuffle(shuffledDeck.cards);
-    printf("After single Fisher-Yates shuffle:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
-
-    deckInit(&shuffledDeck);
-    fisherYatesShuffleMany(shuffledDeck.cards, 7);
-    printf("After 7 Fisher-Yates shuffles:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
-
-    deckInitWithStrategy(&shuffledDeck, overhandShuffle);
-    deckShuffle(&shuffledDeck);
-    printf("After single overhand shuffle with strategy:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
-
-    deckInitWithStrategy(&shuffledDeck, overhandShuffle);
-    deckShuffleMany(&shuffledDeck, 7);
-    printf("After 7 overhand shuffles with strategy:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
-
-    deckInit(&shuffledDeck);
-    shuffle(shuffledDeck.cards, riffleShuffle);
-    printf("After single riffle shuffle with strategy:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
-
-    deckInit(&shuffledDeck);
-    shuffleMany(shuffledDeck.cards, riffleShuffle, 7);
-    printf("After 7 riffle shuffles with strategy:\n");
-    deckShufflingSumary(&originalDeck, &shuffledDeck);
+    Table table;
+    deckInitWithStrategy(&table.deck, fisherYatesShuffle);
+    deckShuffle(&table.deck);
+    deckShufflingSumary(&originalDeck, &table.deck);
+    tablePlayerCardsDeal(&table);
+    tablePlayerCardsPrint(&table);
+    tableFlopDeal(&table);
+    tableCommunalsPrint(&table);
+    tableTurnDeal(&table);
+    tableCommunalsPrint(&table);
+    tableRiverDeal(&table);
+    tableCommunalsPrint(&table);
+    tableReset(&table);
+    tableCommunalsPrint(&table);
 
     return 0;
 }
